cdcc30a637782bda0a32563758f8f9efd99a15f1
Juan Esteban Hoyos <jehz.14@hotmail.com>
2025-09-22 21:19:08 -0500

    Version con validador de inicio de sesión

---
+++ loginApp/src/App.jsx
@@ -0,0 +1,30 @@
+import Login from './pages/login.jsx'
+import Register from './pages/register.jsx'
+import Dashboard from './pages/dashboard.jsx'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import { useState } from 'react';
+
+function App() {
+  
+  let [isAuthenticated, setIsAuthenticated] = useState(false);
+  let handleLogin =()=>{
+    setIsAuthenticated(true);
+  }
+  return (
+    <>
+      <Routes>
+        <Route path='/login' element={ isAuthenticated ? <Navigate to="/dashboard" /> : <Login onLogin={handleLogin}/>} />
+        <Route path='/register' element={ isAuthenticated ? <Navigate to="/dashboard" /> : <Register onLogin={handleLogin}/>} />
+        <Route path='/dashboard' element={ isAuthenticated ? <Dashboard/> : <Navigate to="/login" />} />
+        <Route path='*' element={ <Login onLogin={handleLogin} />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
+
+
+
+
+
