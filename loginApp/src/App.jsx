fa8df71614483f1407162d64737e741e03cc3d9d
Juan Esteban <jehz.14@hotmail.com>
2025-09-23 17:32:32 -0500

    se organiza pagina dashboard y se crea boton logout, tambien se crea funcion logout asociada al boton

--- loginApp/src/App.jsx
+++ loginApp/src/App.jsx
@@ -2,20 +2,34 @@ import Login from './pages/login.jsx'
 import Register from './pages/register.jsx'
 import Dashboard from './pages/dashboard.jsx'
 import { Routes, Route, Navigate } from 'react-router-dom'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
   
-  let [isAuthenticated, setIsAuthenticated] = useState(false);
-  let handleLogin =()=>{
+  let [isAuthenticated, setIsAuthenticated] = useState(localStorage.getItem('isAuthenticated') === 'true');
+  let [userName, setUserName] = useState(localStorage.getItem('userName') || '');
+
+  useEffect(() => {
+    localStorage.setItem('isAuthenticated', isAuthenticated);
+    localStorage.setItem('userName', userName);
+  }, [isAuthenticated, userName]);
+
+  let handleLogin =(userName)=>{
     setIsAuthenticated(true);
+    setUserName(userName);
+  }
+  let handleLogout =()=>{
+    setIsAuthenticated(false);
+    setUserName('');
+    localStorage.removeItem('isAuthenticated');
+    localStorage.removeItem('userName');
   }
   return (
     <>
       <Routes>
         <Route path='/login' element={ isAuthenticated ? <Navigate to="/dashboard" /> : <Login onLogin={handleLogin}/>} />
         <Route path='/register' element={ isAuthenticated ? <Navigate to="/dashboard" /> : <Register />} />
-        <Route path='/dashboard' element={ isAuthenticated ? <Dashboard/> : <Navigate to="/login" />} />
+        <Route path='/dashboard' element={ isAuthenticated ? <Dashboard user={userName} onLogout={handleLogout} /> : <Navigate to="/login" />} />
         <Route path='*' element={ <Login onLogin={handleLogin} />} />
       </Routes>
     </>
